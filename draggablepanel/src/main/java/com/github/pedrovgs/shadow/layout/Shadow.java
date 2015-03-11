e545a53802f903def67c9d46c2444db9731608d4
Pedro Paulo Amorim <pp.amorim@hotmail.com>
2015-03-11 11:12:23 -0300

    shadow removed

--- draggablepanel/src/main/java/com/github/pedrovgs/shadow/layout/Shadow.java
+++
@@ -1,9 +0,0 @@
-package com.github.pedrovgs.shadow.layout;
-
-import android.graphics.Canvas;
-import com.github.pedrovgs.shadow.ZDepthParam;
-
-public interface Shadow {
-  public void setParameter(ZDepthParam parameter, int left, int top, int right, int bottom);
-  public void onDraw(Canvas canvas);
-}
